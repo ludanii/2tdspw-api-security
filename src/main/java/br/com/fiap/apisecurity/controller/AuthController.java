1296f9987281f16856cb0769c82a32bac50445f0
ludanii <ludanielle2005@hotmail.com>
2025-04-29 21:59:00 -0300

    22-04-2025

--- src/main/java/br/com/fiap/apisecurity/controller/AuthController.java
+++ src/main/java/br/com/fiap/apisecurity/controller/AuthController.java
@@ -4,8 +4,8 @@ import br.com.fiap.apisecurity.dto.AuthDTO;
 import br.com.fiap.apisecurity.dto.RegisterDTO;
 import br.com.fiap.apisecurity.model.User;
 import br.com.fiap.apisecurity.repository.UserRepository;
+import br.com.fiap.apisecurity.service.TokenService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,14 +23,17 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthDTO authDTO) {
         var userPwd = new UsernamePasswordAuthenticationToken(
                 authDTO.username(),
                 authDTO.password());
-        //var auth = this.authenticationManager.authenticate(userPwd);
-        return ResponseEntity.ok().build();
+        var auth = this.authenticationManager.authenticate(userPwd);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/register")
--- src/main/java/br/com/fiap/apisecurity/security/SecurityConfiguration.java
+++ src/main/java/br/com/fiap/apisecurity/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.fiap.apisecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private final SecurityFilter securityFilter;
+    @Autowired
+    public SecurityConfiguration(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
             throws Exception {
@@ -29,6 +37,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
